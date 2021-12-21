8691e7bc4072ed6831dfbc111228591dea2351b2
Oliver Scott <olivercscott@gmail.com>
2021-12-21 12:54:03 +0100

    Add preference key for global vpn, global
    Change-Id: Ie5d0a8e89e2ba0254f4d8e2d3ab944c3c274d7dc
    Signed-off-by: Wrdn28 <ifloraxd@gmail.com>

--- sdk/src/java/lineageos/providers/LineageSettings.java
+++ sdk/src/java/lineageos/providers/LineageSettings.java
@@ -3450,6 +3450,12 @@ public final class LineageSettings {
         public static final String WIFI_AUTO_PRIORITIES_CONFIGURATION = "wifi_auto_priority";
 
         /**
+         * Package designated as global VPN provider.
+         * @hide
+         */
+        public static final String GLOBAL_VPN_APP = "global_vpn_app";
+
+        /**
          * Restrict USB
          * 0 = Off, never
          * 1 = Only when the screen is locked
@@ -3491,6 +3497,8 @@ public final class LineageSettings {
         public static final Map<String, Validator> VALIDATORS =
                 new ArrayMap<String, Validator>();
         static {
+            VALIDATORS.put(GLOBAL_VPN_APP,
+                    value -> value.isEmpty() || PACKAGE_NAME_VALIDATOR.validate(value));
             VALIDATORS.put(TRUST_RESTRICT_USB, TRUST_RESTRICT_USB_VALIDATOR);
             VALIDATORS.put(__MAGICAL_TEST_PASSING_ENABLER,
                     __MAGICAL_TEST_PASSING_ENABLER_VALIDATOR);
