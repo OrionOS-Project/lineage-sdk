5d7d2f048cb95e774dd9fc524bad677c680f35de
ezio84 <brabus84@gmail.com>
2025-04-21 20:10:13 +0200

    PowerMenuConstants: Add restart systemui shortcut to power menu [2/2]
    Google made it very easy on this cycle (systemui can just kill its own pid).
    
    Change-Id: I91021e906907737c47b09da57c0181ee23cbcde7
    [nurkeinneid] partial pick of DirtyUnicorns/android_frameworks_base@9c53066
    Signed-off-by: NurKeinNeid <nurkeinneid@derpfest.org>
    Signed-off-by: Wrdn28 <ifloraxd@gmail.com>

--- sdk/src/java/org/lineageos/internal/util/PowerMenuConstants.java
+++ sdk/src/java/org/lineageos/internal/util/PowerMenuConstants.java
@@ -24,6 +24,7 @@ public class PowerMenuConstants {
     public static final String GLOBAL_ACTION_KEY_DEVICECONTROLS = "devicecontrols";
     public static final String GLOBAL_ACTION_KEY_SYSTEM_UPDATE = "system_update";
     public static final String GLOBAL_ACTION_KEY_STANDBY = "standby";
+    public static final String GLOBAL_ACTION_KEY_RESTART_SYSTEMUI = "restart_systemui";
 
     /**
      * Advanced restart menu actions
@@ -47,6 +48,7 @@ public class PowerMenuConstants {
         GLOBAL_ACTION_KEY_VOICEASSIST,
         GLOBAL_ACTION_KEY_ASSIST,
         GLOBAL_ACTION_KEY_DEVICECONTROLS,
+        GLOBAL_ACTION_KEY_RESTART_SYSTEMUI,
         GLOBAL_ACTION_KEY_LOGOUT,
         GLOBAL_ACTION_KEY_SYSTEM_UPDATE,
         GLOBAL_ACTION_KEY_STANDBY,
